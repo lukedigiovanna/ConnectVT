0464e08c784dc90b12c9b2ef1d690c72eccec7d9
lukedigiovanna <43895803+lukedigiovanna@users.noreply.github.com>
2022-04-26 20:29:19 -0400

    Add registration page content

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -6,7 +6,36 @@ function Register() {
     return(
         <main className="register">
             <section className='content-form'>
-                
+                <div className='inner-content'>
+                    <h1>
+                        Sign Up
+                    </h1>
+
+                    <form>
+                        <div className='form-group'>
+                            <label htmlFor='name'>
+                                Username
+                            </label>
+                            <input type='text' id='name' className='form-control' />
+                        </div>
+                        <div className='form-group'>
+                            <label htmlFor='email'>
+                                Email
+                            </label>
+                            <input type='text' id='email' className='form-control' />
+                        </div>
+                        <div className='form-group'>
+                            <label htmlFor='password'>
+                                Password
+                            </label>
+                            <input type='password' id='password' className='form-control' />
+                        </div>
+
+                        <button type="button" className="button">
+                            Sign Up
+                        </button>
+                    </form>
+                </div>
             </section>
         </main>
     )
